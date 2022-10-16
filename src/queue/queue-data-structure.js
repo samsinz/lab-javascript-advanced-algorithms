a003acd49fcc231750abd36ff524c24577560617
Sam Sinz <sam@sinz.fr>
2022-10-16 23:16:20 +0200

    done, dom doesn't really work though

--- src/queue/queue-data-structure.js
+++ src/queue/queue-data-structure.js
@@ -5,25 +5,34 @@ class Queue {
   }
 
   canEnqueue() {
-    // ... your code goes here
+    return this.queueControl.length !== this.MAX_SIZE;
   }
 
   isEmpty() {
-    // ... your code goes here
+    return this.queueControl.length === 0;
   }
 
   enqueue(item) {
-    // ... your code goes here
+    if (this.canEnqueue()) {
+      this.queueControl.push(item);
+      return this.queueControl;
+    } else {
+      throw new Error("QUEUE_OVERFLOW");
+    }
   }
 
   dequeue() {
-    // ... your code goes here
+    if (this.isEmpty()) {
+      throw new Error("QUEUE_UNDERFLOW");
+    } else {
+      return this.queueControl.splice(0, 1)[0];
+    }
   }
 
   display() {
-    // ... your code goes here
-  }  
+    return this.queueControl;
+  }
 }
 
 // This is required to enable the automated tests, please ignore it.
-if (typeof module !== 'undefined') module.exports = Queue;
+if (typeof module !== "undefined") module.exports = Queue;
--- src/stack/stack-dom.js
+++ src/stack/stack-dom.js
@@ -1,51 +1,53 @@
-const stackList = document.getElementById('stack-list');
-const stackInput = document.getElementById('stack-input');
-const container = document.getElementById('container');
-const warningTopStack = document.querySelector('#stack-container .warning-top');
-const warningBottomStack = document.querySelector('#stack-container .warning-bottom');
-const addStackBtn = document.getElementById('add-stack');
-const takeStackBtn = document.getElementById('take-stack');
+const stackList = document.getElementById("stack-list");
+const stackInput = document.getElementById("stack-input");
+const container = document.getElementById("container");
+const warningTopStack = document.querySelector("#stack-container .warning-top");
+const warningBottomStack = document.querySelector(
+  "#stack-container .warning-bottom"
+);
+const addStackBtn = document.getElementById("add-stack");
+const takeStackBtn = document.getElementById("take-stack");
 
 const newStack = new Stack();
 
 const clearStackInput = () => {
-  stackInput.value = '';
+  stackInput.value = "";
 };
 
 const renderListStack = () => {
-  warningTopStack.style.display = 'none';
-  warningBottomStack.style.display = 'none';
-  stackList.innerHTML = '';
+  warningTopStack.style.display = "none";
+  warningBottomStack.style.display = "none";
+  stackList.innerHTML = "";
   let length = newStack.display().length;
   let size = newStack.MAX_SIZE - length;
-  newStack.display().forEach(item => {
-    let li = document.createElement('li');
-    li.className = 'active';
+  newStack.display().forEach((item) => {
+    let li = document.createElement("li");
+    li.className = "active";
     li.innerText = item;
     stackList.appendChild(li);
   });
   for (let i = 0; i < size; i++) {
-    let li = document.createElement('li');
-    li.className = 'inactive';
-    li.innerHTML = '&nbsp;';
+    let li = document.createElement("li");
+    li.className = "inactive";
+    li.innerHTML = "&nbsp;";
     stackList.appendChild(li);
   }
 };
 renderListStack();
 
-const generateWarningStack = type => {
-  if (type === 'underflow') {
-    warningBottomStack.style.display = 'block';
+const generateWarningStack = (type) => {
+  if (type === "underflow") {
+    warningBottomStack.style.display = "block";
     warningBottomStack.innerText = type;
-  } else if (type === 'overflow') {
-    warningTopStack.style.display = 'block';
+  } else if (type === "overflow") {
+    warningTopStack.style.display = "block";
     warningTopStack.innerText = type;
   }
 };
 
 const addToStack = () => {
-  if (newStack.push(stackInput.value) === 'Stack Overflow') {
-    generateWarningStack('overflow');
+  if (newStack.push(stackInput.value) === "Stack Overflow") {
+    generateWarningStack("overflow");
   } else {
     clearStackInput();
     renderListStack();
@@ -53,12 +55,12 @@ const addToStack = () => {
 };
 
 const removeFromStack = () => {
-  if (newStack.pop() === 'Stack Underflow') {
-    generateWarningStack('underflow');
+  if (newStack.pop() === "Stack Underflow") {
+    generateWarningStack("underflow");
   } else {
     renderListStack();
   }
 };
 
-addStackBtn.addEventListener('click', addToStack);
-takeStackBtn.addEventListener('click', removeFromStack);
+addStackBtn.addEventListener("click", addToStack);
+takeStackBtn.addEventListener("click", removeFromStack);
